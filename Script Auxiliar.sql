5b415e7b682b6ee5199e03c9cb908ad6528d5dcd
Matias Ezequiel Gonzalez Samperi <matias_gonza14@hotmail.com>
2020-11-18 21:35:17 -0300

    Actualizacion de script sql

--- Script Auxiliar.sql
+++ Script Auxiliar.sql
@@ -6,23 +6,6 @@ use TPC_GONZALEZ_JESUS
 
 
 
-insert into CLASIFICACION (clase,rubro,nombre) values ('INCIDENTE','INCIDENTE','Inconveniente con RAM')
-
-insert into persona (apellido,nombre,fecha_nacimiento,DNI) values  
-	('GONZALEZ','MATIAS','1993-05-16', 111),
-	('JESUS','CARLOS','1994-10-07', 222)
-
-
-INSERT into ESTADOS (estado) values ('NUEVO'),('EN COLA'),('EN PROGRESO'),('RESUELTO'),('CERRADO'),('CANCELADO')
-
-insert into TICKET (clase,descripcion,detalle,estado,urgencia,clasificacionid) VALUES ('INCIDENTE','Pantallazo azul','El cliente reporta que tiene repetidos pantallazos azul indicando memoria insuficiente','NUEVO',3,1)
-
-
-
-insert into GrupoPersonas (idpersona,idarea) values (111,1), (222,1)
-insert into TICKET (clase,descripcion,detalle,estado,urgencia,clasificacionid,creadopor) VALUES ('INCIDENTE','Pantallazo azul','El cliente reporta que tiene repetidos pantallazos azul indicando memoria insuficiente','NUEVO',3,1,111111)
-
-
 
 /* ===============00 */
 
@@ -31,18 +14,13 @@ insert into TICKET (clase,descripcion,detalle,estado,urgencia,clasificacionid,cr
 select SCOPE_IDENTITY() as newid 
 
 
+go
+USE MASTER
 
+go
 
-CREATE PROCEDURE sp_crearIncidenteNuevo(
-	@descripcion varchar(300),
-	@detalle varchar ( 3000),
-	@urgencia int ,
-	@clasificacionid int,
-	@creadopor int
-	
-)
-AS
-begin 
-	insert into ticket (clase,descripcion,detalle,estado,urgencia,clasificacionid,creadopor) 
-	VALUES ('INCIDENTE', 'Apagado automatico',' UN cliente reporta que su pc se apaga sola','NUEVO',2,1,222)
+exec sp_crearIncidenteNuevo 'nueva desc','larga desc',3,1,222
+
+select * from ticket
+
 
--- TPC_Gonzalez-Jesus.sql
+++ TPC_Gonzalez-Jesus.sql
@@ -1,18 +1,31 @@
+
+
+
+
+/* ============   ============   ============  ============   ============   ============  ============   ============   ============ */
+/* ============									 LIMPIEZA															     ============ */
+/* ============   ============   ============  ============   ============   ============  ============   ============   ============ */
 use master 
 go
 
+if DB_ID('TPC_GONZALEZ_JESUS') is not null 
 drop database TPC_GONZALEZ_JESUS
 GO
 
-use master
-GO
---create database TICKET_IT
 create database TPC_GONZALEZ_JESUS
 GO
---use TICKET_IT
 USE TPC_GONZALEZ_JESUS
 GO
 
+
+
+/* ============   ============   ============  ============   ============   ============  ============   ============   ============ */
+/* ============																											 ============ */
+/* ============									 TABLAS																     ============ */
+/* ============																											 ============ */
+/* ============   ============   ============  ============   ============   ============  ============   ============   ============ */
+
+-- Tabla principal de personas identificadas por su DNI.
 create table PERSONA (
 	apellido varchar(100) not null,
 	nombre varchar(100) not null,  
@@ -21,9 +34,19 @@ create table PERSONA (
 	DNI int NOT NULL PRIMARY KEY,
 	cliente bit default 0
 	)
-
+	
+	/* ============   ============   ============ */
+	-- Informacion de contacto de X persona.
+create table INFOCONTACTO(
+	infoconcactoid int identity(1,1) primary key,
+	idpersona int not null unique foreign key references persona(DNI),  /* evaluar si mantener DNI o usar una clumna ID */
+	telefono varchar(10),
+	mail varchar(30),
+	direccion varchar (50)
+	)
 	/* ============   ============   ============ */
 
+-- Tabla de Areas, identifica las areas laborales de la empresa o clientes.
 create table AREAS(
 	areasID  TINYINT IDENTITY(1,1) not null PRIMARY KEY ,
 	nombre varchar(50) UNIQUE
@@ -32,25 +55,16 @@ INSERT into AREAS (nombre) values ('TECNICO'),('ADMINISTRATIVO'),('RRHH'),('FINA
 
 
 	/* ============   ============   ============ */
-
+-- Grupo de personas para asignar trabajo en tratamiento de tickets
 create table GrupoPersonas (
 	grupopersonasid int IDENTITY(1,1) primary key ,
 	idpersona int not null unique foreign key references persona(DNI),  /* evaluar si mantener DNI o usar una clumna ID */
 	idarea tinyint not null foreign key references AREAS(areasID)
 )
 
-	/* ============   ============   ============ */
-
-create table INFOCONTACTO(
-	infoconcactoid int identity(1,1) primary key,
-	idpersona int not null unique foreign key references persona(DNI),  /* evaluar si mantener DNI o usar una clumna ID */
-	telefono varchar(10),
-	mail varchar(30),
-	direccion varchar (50)
-	)
 
 	/* ============   ============   ============ */
-
+	-- Tabla de clasificaciones para asignar la naturaleza de un ticket o el tipo de un Activo
 create table CLASIFICACION(
 	clasificacionid int identity(1,1) primary key,
 	clase varchar(20) not null, /* Define para que tipo de ticket aplica, o en su defecto si es de activos */
@@ -63,13 +77,14 @@ create table CLASIFICACION(
 	
 
 	/* ============   ============   ============ */
+	/* Define los distintos estados posibles tanto de tickets como de activos y personas */
 
-create table ESTADOS(		/* Define los distintos estados posibles tanto de tickets como de activos y personas */
+create table ESTADOS(		
 	estadoid int identity(1,1) primary key,
 	estado varchar(20) not null, /* Define para que tipo de ticket aplica */
 	dominio varchar(20) not null
 
-	constraint Chk_estado_dominio check (dominio in ('TICKET','ACTIVO','PERSONA'))	
+	constraint Chk_estado_dominio check (dominio in ('TICKET','ACTIVO','PERSONA','INCIDENTE','PROBLEMA','OT','SOLICITUD'))	
 	)
 
 	
@@ -82,12 +97,14 @@ create table ESTADOS(		/* Define los distintos estados posibles tanto de tickets
 --	)
 
 	/* ============   ============   ============ */
+
+	--Tabla principal de Tickets
 create table TICKET (
 	--ticketuid int identity(1,1) primary key,	/* identificador unico para todos los tickets */
 	ticketid int identity(1,1) primary key,		/* identificador para cada ticket de su propia clase */
 	--clase varchar(20) not null foreign key REFERENCES CLASETICKET(claseticketid),
 	clase varchar(20) not null ,
-	fecha_creacion date default CURRENT_TIMESTAMP,
+	fecha_creacion datetime default CURRENT_TIMESTAMP,
 	fecha_fin date,
 	descripcion varchar(300) not null,
 	estado varchar(20) not null default 'NUEVO',
@@ -104,17 +121,20 @@ create table TICKET (
 	constraint Chk_urgencia check (urgencia in (1,2,3,4,5))
 	)
 
+	
 	/* ============   ============   ============ */
+
+	-- Historial de estado de los tickets
 create table tkstatushistory(
 	tkstatushistory int identity(1,1) primary key,
 	estado varchar(20) not null,
-	fecha timestamp not null default CURRENT_TIMESTAMP,
+	fecha datetime not null default CURRENT_TIMESTAMP,
 	propietario int not null foreign key REFERENCES PERSONA(DNI)
 )
 
 	/* ============   ============   ============ */
-
-create table ACTIVOS (  /* INVENTARIO */
+	/* INVENTARIO */
+create table ACTIVOS (  
 	activosid int identity(1,1) primary key,
 	nombre varchar(50) not null,
 	descripcion varchar(300),
@@ -124,12 +144,88 @@ create table ACTIVOS (  /* INVENTARIO */
 	costo money not null default 0	/* Costo de cara a la venta del cliente */
 
 	)
-
-
 	/* ============   ============   ============ */
---select * from persona
+	-- Tabla de registros de trabajo sobre un ticket para dejar notas
+create table REGISTRO(
+	registroid int identity(1,1) primary key,
+	descripcion varchar (300) not null,
+	detalle varchar (3000),
+	clase varchar (20), -- identificador del tipo de objeto padre
+	ticketid int foreign key REFERENCES ticket(ticketid),
+	fecha_creacion datetime not null default CURRENT_TIMESTAMP,
+	creadopor int not null foreign key REFERENCES PERSONA(DNI)
+	)
 
---select * from grupopersonas
+go
+	
+/* ============   ============   ============  ============   ============   ============  ============   ============   ============ */
+/* ============									DATOS DE PRUEBA														     ============ */
+/* ============   ============   ============  ============   ============   ============  ============   ============   ============ */
 
 
---select * from ESTADOS
+
+insert into CLASIFICACION (clase,rubro,nombre) values ('INCIDENTE','INCIDENTE','Inconveniente con RAM')
+
+insert into persona (apellido,nombre,fecha_nacimiento,DNI) values  
+	('GONZALEZ','MATIAS','1993-05-16', 111),
+	('JESUS','CARLOS','1994-10-07', 222)
+
+
+INSERT into ESTADOS (estado,dominio) values ('NUEVO','INCIDENTE'),('EN COLA','INCIDENTE'),('EN PROGRESO','INCIDENTE'),('RESUELTO','INCIDENTE'),('CERRADO','INCIDENTE'),('CANCELADO','INCIDENTE')
+
+insert into TICKET (clase,descripcion,detalle,estado,urgencia,clasificacionid,creadopor) 
+VALUES ('INCIDENTE','Pantallazo azul','El cliente reporta que tiene repetidos pantallazos azul indicando memoria insuficiente','NUEVO',3,1,222)
+
+
+
+insert into GrupoPersonas (idpersona,idarea) values (111,1), (222,1)
+
+
+go
+
+
+
+	/* ============   ============   ============  ============   ============   ============  ============   ============   ============ */
+	/* ============																											 ============ */
+	/* ============									 PROCEDIMIENTOS ALMACENADOS											     ============ */
+	/* ============																											 ============ */
+	/* ============   ============   ============  ============   ============   ============  ============   ============   ============ */
+begin if (OBJECT_ID(N'sp_crearIncidenteNuevo') is not null) 
+drop procedure sp_crearIncidenteNuevo end
+
+go
+
+
+CREATE PROCEDURE sp_crearIncidenteNuevo(
+	@descripcion varchar(300),	@detalle varchar ( 3000),	@urgencia int ,	@clasificacionid int,	@creadopor int	
+)
+AS
+begin 
+	declare @succes bit=0
+	begin try
+		insert into [TICKET] (clase,descripcion,detalle,estado,urgencia,clasificacionid,creadopor) 
+		VALUES ('INCIDENTE', @descripcion,@detalle,'NUEVO',@urgencia,@clasificacionid,@creadopor)
+
+		set @succes = case when @@rowcount =0 then 0 else 1 end 
+		declare @ticketid int = SCOPE_IDENTITY() -- Asigna el valor a la ultima PK creada, en este caso el ticketID 
+
+		insert into [tkstatushistory] (ticketid,estado,clase)
+		values (@ticketid,'NUEVO','INCIDENTE')
+	end try
+	begin catch
+		set @succes=0
+	end catch
+	if (@succes = 1)
+	begin 
+		select @ticketid as Ticketid
+		--select * from ticket where ticketid = @ticketid
+	end
+	else
+	begin
+		select 'Error' as Mensaje -- Definir mensaje de error
+	end
+
+end
+
+
+select * from ticket
